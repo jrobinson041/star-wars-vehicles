c575327b5b3bd9e228f09a86154d036084a51f5c
John Robinson <jrobinson041@gmail.com>
2022-05-17 00:57:43 -0400

    more responive appbar

--- src/Components/AppBar.tsx
+++ src/Components/AppBar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { AppBar as MuiAppBar, Grid, Typography } from "@mui/material";
+import {
+  AppBar as MuiAppBar,
+  Grid,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const AppBar = () => {
+  const theme = useTheme();
+  const large = useMediaQuery(theme.breakpoints.up("md"));
+
   return (
     <MuiAppBar style={styles.appBar} enableColorOnDark position="sticky">
       <Grid container alignItems="center" spacing={3}>
@@ -12,23 +21,25 @@ const AppBar = () => {
             style={styles.logo}
           />
         </Grid>
-        <Grid item>
-          <Typography display="inline" variant="h5" style={styles.title}>
+        <Grid item style={{ display: "relative" }}>
+          <Typography variant="h5" style={styles.title}>
             Non-Hyperdrive Vehicle Database
           </Typography>
         </Grid>
-        <Grid item>
-          <Typography
-            variant="caption"
-            color="textSecondary"
-            component="a"
-            href="https://swapi.dev/"
-            target="_blank"
-            style={styles.link}
-          >
-            A Star Wars API Saga
-          </Typography>
-        </Grid>
+        {large && (
+          <Grid item>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              component="a"
+              href="https://swapi.dev/"
+              target="_blank"
+              style={styles.link}
+            >
+              A Star Wars API Saga
+            </Typography>
+          </Grid>
+        )}
       </Grid>
     </MuiAppBar>
   );
